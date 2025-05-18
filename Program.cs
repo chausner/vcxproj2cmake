86b7595631e6ae50adffeb28363eac678aec94a9
Christoph Hausner <christoph_hausner@hotmail.de>
2025-05-18 20:48:17 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,280 @@
+﻿using Scriban;
+using System.CommandLine;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using vcxproj2cmake;
+
+static partial class Program
+{
+    static int Main(string[] args)
+    {
+        var projectOption = new Option<string>(
+            name: "--project",
+            description: "Path to .vcxproj file")
+        { IsRequired = true };
+
+        var rootCommand = new RootCommand("Converts VC++ projects to CMake");
+        rootCommand.AddOption(projectOption);
+        rootCommand.SetHandler(Run, projectOption);
+
+        return rootCommand.Invoke(args);
+    }
+
+    static void Run(string project)
+    {
+        var conanPackageInfo = LoadConanPackageInfo();
+
+        var projectFileInfo = ParseProjectFile(project, conanPackageInfo);
+
+        var template = Template.Parse(Templates.CMakeLists);
+        var result = template.Render(projectFileInfo);
+
+        Console.WriteLine(result);
+    }
+
+    static Dictionary<string, ConanPackage> LoadConanPackageInfo()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        using var stream = assembly.GetManifestResourceStream("vcxproj2cmake.conan-packages.csv");
+        using var streamReader = new StreamReader(stream);
+
+        return
+            streamReader.ReadToEnd()
+            .Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(line => line.Split(','))
+            .Select(tokens => (tokens[0], new ConanPackage(tokens[1], tokens[2])))
+            .ToDictionary();
+    }
+
+    static ProjectFileInfo ParseProjectFile(string project, Dictionary<string, ConanPackage> conanPackageInfo)
+    {
+        var msbuildNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
+        var projectXName = XName.Get("Project", msbuildNamespace);
+        var propertyGroupXName = XName.Get("PropertyGroup", msbuildNamespace);
+        var configurationTypeXName = XName.Get("ConfigurationType", msbuildNamespace);
+        var itemGroupXName = XName.Get("ItemGroup", msbuildNamespace);
+        var clCompileXName = XName.Get("ClCompile", msbuildNamespace);
+        var projectConfigurationXName = XName.Get("ProjectConfiguration", msbuildNamespace);
+        var itemDefinitionGroupXName = XName.Get("ItemDefinitionGroup", msbuildNamespace);
+        var linkXName = XName.Get("Link", msbuildNamespace);
+        var qtModulesXName = XName.Get("QtModules", msbuildNamespace);
+        var importXName = XName.Get("Import", msbuildNamespace);
+        var importGroupXName = XName.Get("ImportGroup", msbuildNamespace);
+
+        var doc = XDocument.Load(project);
+        var projectElement = doc.Element(projectXName);
+
+        var projectConfigurations =
+            projectElement
+            .Elements(itemGroupXName)
+            .SelectMany(group => group.Elements(projectConfigurationXName))
+            .Select(element => element.Attribute("Include")!.Value)
+            .ToList();
+
+        var configurationType =
+            projectElement
+            .Elements(propertyGroupXName)
+            .SelectMany(group => group.Elements(configurationTypeXName))
+            .Select(element => element.Value)
+            .Distinct()
+            .Single();
+
+        var sourceFiles =
+            projectElement
+            .Elements(itemGroupXName)
+            .SelectMany(group => group.Elements(clCompileXName))
+            .Select(element => element.Attribute("Include")!.Value)
+            .ToList();
+
+        var qtModules =
+            (projectElement
+            .Elements(propertyGroupXName)
+            .SelectMany(group => group.Elements(qtModulesXName))
+            .Select(element => element.Value)
+            .Distinct()
+            .SingleOrDefault() ?? "").Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        var imports =
+            projectElement
+            .Elements(importXName)
+            .Concat(projectElement.Elements(importGroupXName).SelectMany(group => group.Elements(importXName)))
+            .Select(import => import.Attribute("Project")!.Value)
+            .ToList();
+
+        Dictionary<string, Dictionary<string, string>> compilerSettings = [];
+        Dictionary<string, Dictionary<string, string>> linkerSettings = [];
+
+        foreach (var projectConfig in projectConfigurations)
+        {
+            if (!Regex.IsMatch(projectConfig, @"(Debug|Release)\|(Win32|x86|x64)"))
+            {
+                Console.WriteLine($"Warning: skipping unsupported project configuration: {projectConfig}");
+                continue;
+            }
+
+            var itemDefinitionGroups =
+                projectElement
+                .Elements(itemDefinitionGroupXName)
+                .Where(group => group.Attribute("Condition") == null ||
+                    group.Attribute("Condition")!.Value == $"'$(Configuration)|$(Platform)'=='{projectConfig}'")
+                .ToList();
+
+            foreach (var group in itemDefinitionGroups)
+            {
+                var projectConfigCompilerSettings =
+                    group
+                    .Elements(clCompileXName)
+                    .SelectMany(element => element.Elements())
+                    .ToDictionary(element => element.Name.LocalName, element => element.Value);
+
+                var projectConfigLinkerSettings =
+                    group
+                    .Elements(linkXName)
+                    .SelectMany(element => element.Elements())
+                    .ToDictionary(element => element.Name.LocalName, element => element.Value);
+
+                foreach (var setting in projectConfigCompilerSettings)
+                {
+                    compilerSettings.TryAdd(setting.Key, []);
+                    compilerSettings[setting.Key][projectConfig] = setting.Value;
+                }
+
+                foreach (var setting in projectConfigLinkerSettings)
+                {
+                    linkerSettings.TryAdd(setting.Key, []);
+                    linkerSettings[setting.Key][projectConfig] = setting.Value;
+                }
+            }
+        }
+
+        var languageStandard = compilerSettings.GetValueOrDefault("LanguageStandard")?.Values.Distinct().SingleOrDefault();
+        if (languageStandard == null)
+            Console.WriteLine("Warning: Language standard could not be determined.");
+
+        var includePaths = ConfigDependentSetting.Parse(
+            compilerSettings.GetValueOrDefault("AdditionalIncludeDirectories"), ParseIncludePaths);
+
+        var libraries = ConfigDependentSetting.Parse(
+            compilerSettings.GetValueOrDefault("AdditionalDependencies"), ParseLibraries);
+
+        var defines = ConfigDependentSetting.Parse(
+            compilerSettings.GetValueOrDefault("PreprocessorDefinitions"), ParseDefines);
+
+        var options = ConfigDependentSetting.Parse(
+            compilerSettings.GetValueOrDefault("AdditionalOptions"), ParseOptions);
+
+        var conanPackages =
+            imports
+            .Select(import =>
+            {
+                var match = Regex.Match(import, @"conan_([A-Za-z0-9-_]+)\.props");
+                return match.Success ? match.Groups[1].Value : null;
+            })
+            .Where(packageName => packageName != null)
+            .Select(packageName => conanPackageInfo.GetValueOrDefault(packageName, new ConanPackage(packageName, packageName)))
+            .ToArray();
+
+        return new ProjectFileInfo
+        {
+            ProjectName = Path.GetFileNameWithoutExtension(project),
+            ConfigurationType = configurationType,
+            LanguageStandard = languageStandard,
+            SourceFiles = sourceFiles.ToArray(),
+            IncludePaths = includePaths,
+            Libraries = libraries,
+            Defines = defines,
+            Options = options,
+            QtModules = qtModules,
+            ConanPackages = conanPackages
+        };
+    }
+
+    static string[] ParseList(string list, char separator, string placeholder)
+    {
+        return list
+            .Split(separator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Except([placeholder])
+            .Distinct()
+            .ToArray();
+    }
+
+    static string[] ParseIncludePaths(string includePaths) => ParseList(includePaths, ';', "%(AdditionalIncludeDirectories)");
+
+    static string[] ParseLibraries(string libraries) => ParseList(libraries, ';', "%(AdditionalDependencies)");
+
+    static string[] ParseDefines(string defines) => ParseList(defines, ';', "%(PreprocessorDefinitions)");
+
+    static string[] ParseOptions(string options) => ParseList(options, ' ', "%(AdditionalOptions)");
+}
+
+class ProjectFileInfo
+{
+    public required string ProjectName { get; set; }
+    public required string ConfigurationType { get; set; }
+    public required string? LanguageStandard { get; set; }
+    public required string[] SourceFiles { get; set; }
+    public required ConfigDependentSetting IncludePaths { get; set; }
+    public required ConfigDependentSetting Libraries { get; set; }
+    public required ConfigDependentSetting Defines { get; set; }
+    public required ConfigDependentSetting Options { get; set; }
+    public required string[] QtModules { get; set; }
+    public required ConanPackage[] ConanPackages { get; set; }
+}
+
+class ConfigDependentSetting
+{
+    public required string[] Common { get; set; }
+    public required string[] Debug { get; set; }
+    public required string[] Release { get; set; }
+    public required string[] X86 { get; set; }
+    public required string[] X64 { get; set; }
+
+    public static readonly ConfigDependentSetting Empty = new()
+    {
+        Common = [],
+        Debug = [],
+        Release = [],
+        X86 = [],
+        X64 = []
+    };
+
+    public static ConfigDependentSetting Parse(Dictionary<string, string>? settings, Func<string, string[]> parser)
+    {
+        if (settings == null || settings.Count == 0)
+            return Empty;
+
+        var parsedSettings = settings.ToDictionary(kvp => kvp.Key, kvp => parser(kvp.Value));
+
+        var allSettingValues = parsedSettings.Values.SelectMany(s => s).Distinct().ToArray();
+
+        var commonSettingValues = allSettingValues.Where(s => parsedSettings.All(kvp => kvp.Value.Contains(s))).ToArray();
+
+        string[] FilterValues(Func<string, bool> selector)
+        {
+            return allSettingValues
+                .Where(s => parsedSettings.All(kvp => selector(kvp.Key) == kvp.Value.Contains(s)))
+                .Except(commonSettingValues)
+                .ToArray();
+        }
+
+        var result = new ConfigDependentSetting
+        {
+            Common = commonSettingValues,
+            Debug = FilterValues(config => config.StartsWith("Debug|")),
+            Release = FilterValues(config => config.StartsWith("Release|")),
+            X86 = FilterValues(config => config.EndsWith("|Win32") || config.EndsWith("|x86")),
+            X64 = FilterValues(config => config.EndsWith("|x64"))
+        };
+
+        var skippedSettings = parsedSettings.Values.SelectMany(s => s).Except(result.Common).Except(result.Debug).Except(result.Release).Except(result.X86).Except(result.X64).ToArray();
+        if (skippedSettings.Length > 0)
+            Console.WriteLine($"Warning: some settings were skipped: {string.Join(", ", skippedSettings)}");
+
+        return result;
+    }
+
+    public bool IsEmpty => Common.Length == 0 && Debug.Length == 0 && Release.Length == 0 && X86.Length == 0 && X64.Length == 0;
+}
+
+record ConanPackage(string CMakeConfigName, string CMakeTargetName);
---
+++ Templates.cs
@@ -0,0 +1,125 @@
+﻿namespace vcxproj2cmake;
+
+static class Templates
+{
+    public const string CMakeLists = """
+cmake_minimum_required(VERSION 3.0)
+
+project({{ project_name }})
+
+{{~ if qt_modules.size > 0 ~}}
+find_package(Qt5 REQUIRED COMPONENTS {{ qt_modules | array.join " " }})
+{{~ end -}}
+{{~ for package in conan_packages ~}}
+find_package({{ package.cmake_config_name }} REQUIRED CONFIG)
+{{~ end ~}}
+
+{{~ if configuration_type == "Application" ~}}
+add_executable({{ project_name }}
+{{~ else if configuration_type == "StaticLibrary" ~}}
+add_library({{ project_name }} STATIC
+{{~ else if configuration_type == "DynamicLibrary" ~}}
+add_library({{ project_name }} SHARED
+{{~ else ~}}
+{{ error.unsupported_configuration_type }}
+{{~ end ~}}
+    {{ source_files | array.sort | array.join "\n" }}
+)
+
+{{~ if language_standard ~}}
+target_compile_features({{ project_name }} PUBLIC {{ }}
+    {{- if language_standard == "stdcpp20" -}} cxx_std_20
+    {{- else if language_standard == "stdcpp17" -}} cxx_std_17
+    {{- else if language_standard == "stdcpp14" -}} cxx_std_14
+    {{- else if language_standard == "stdcpp11" -}} cxx_std_11
+    {{- else -}}
+    {{ error.unsupported_language_standard }}
+    {{- end -}}
+)
+
+{{~ end -}}
+
+{{~ if !include_paths.is_empty ~}}
+target_include_directories({{ project_name }} PUBLIC
+    {{ include_paths.common | array.join "\n" }}
+    {{~ for path in include_paths.debug ~}}
+    $<$<CONFIG:Debug>:{{ path }}>
+    {{~ end ~}}
+    {{~ for path in include_paths.release ~}}
+    $<$<CONFIG:Release>:{{ path }}>
+    {{~ end ~}}
+    {{~ for path in include_paths.x86 ~}}
+    $<$<STREQUAL:$<TARGET_PROPERTY:ARCHITECTURE_ID>,x86>:{{ path }}>
+    {{~ end ~}}
+    {{~ for path in include_paths.x64 ~}}
+    $<$<STREQUAL:$<TARGET_PROPERTY:ARCHITECTURE_ID>,x64>:{{ path }}>
+    {{~ end ~}}
+)
+
+{{~ end -}}
+
+{{~ if !defines.is_empty ~}}
+target_compile_definitions({{ project_name }} PUBLIC
+    {{ defines.common | array.join "\n" }}
+    {{~ for define in defines.debug ~}}
+    $<$<CONFIG:Debug>:{{ define }}>
+    {{~ end ~}}
+    {{~ for define in defines.release ~}}
+    $<$<CONFIG:Release>:{{ define }}>
+    {{~ end ~}}
+    {{~ for define in defines.x86 ~}}
+    $<$<STREQUAL:$<TARGET_PROPERTY:ARCHITECTURE_ID>,x86>:{{ define }}>
+    {{~ end ~}}
+    {{~ for define in defines.x64 ~}}
+    $<$<STREQUAL:$<TARGET_PROPERTY:ARCHITECTURE_ID>,x64>:{{ define }}>
+    {{~ end ~}}
+)
+
+{{~ end -}}
+
+{{~ if !libraries.is_empty || qt_modules.size > 0 ~}}
+target_link_libraries({{ project_name }} PUBLIC
+    {{ libraries.common | array.join "\n" }}
+    {{~ for library in libraries.debug ~}}
+    $<$<CONFIG:Debug>:{{ library }}>
+    {{~ end ~}}
+    {{~ for library in libraries.release ~}}
+    $<$<CONFIG:Release>:{{ library }}>
+    {{~ end ~}}
+    {{~ for library in libraries.x86 ~}}
+    $<$<STREQUAL:$<TARGET_PROPERTY:ARCHITECTURE_ID>,x86>:{{ library }}>
+    {{~ end ~}}
+    {{~ for library in libraries.x64 ~}}
+    $<$<STREQUAL:$<TARGET_PROPERTY:ARCHITECTURE_ID>,x64>:{{ library }}>
+    {{~ end ~}}
+    {{~ for module in qt_modules ~}}
+    Qt5::{{ module }}
+    {{~ end ~}}
+    {{~ for package in conan_packages ~}}
+    {{ package.cmake_target_name }}
+    {{~ end ~}}
+)
+
+{{~ end -}}
+
+{{~ if !options.is_empty ~}}
+target_compile_options({{ project_name }} PUBLIC
+    {{ options.common | array.join "\n" }}
+    {{~ for option in options.debug ~}}
+    $<$<CONFIG:Debug>:{{ option }}>
+    {{~ end ~}}
+    {{~ for option in options.release ~}}
+    $<$<CONFIG:Release>:{{ option }}>
+    {{~ end ~}}
+    {{~ for option in options.x86 ~}}
+    $<$<STREQUAL:$<TARGET_PROPERTY:ARCHITECTURE_ID>,x86>:{{ option }}>
+    {{~ end ~}}
+    {{~ for option in options.x64 ~}}
+    $<$<STREQUAL:$<TARGET_PROPERTY:ARCHITECTURE_ID>,x64>:{{ option }}>
+    {{~ end ~}}
+)
+
+{{~ end -}}
+
+""";
+}
