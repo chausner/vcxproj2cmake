5b502cbc68691e02fdfdbc977f8cf31dde90240d
Christoph Hausner <christoph_hausner@hotmail.de>
2025-05-21 21:18:57 +0200

    Extend NormalizePath

--- Program.cs
+++ Program.cs
@@ -192,8 +192,33 @@ static class Program
 
     static string NormalizePath(string path)
     {
+        if (path == string.Empty)
+            return string.Empty;
+
         // In CMake, we should always use forward-slashes as directory separator, even on Windows
         string normalizedPath = path.Replace(@"\", "/");
+
+        // Remove duplicated separators
+        normalizedPath = Regex.Replace(normalizedPath, @"//+", "/");
+
+        // Remove ./ prefix(es)
+        while (normalizedPath.StartsWith("./"))
+            normalizedPath = normalizedPath.Substring(2);
+        if (normalizedPath == string.Empty)
+            return ".";
+
+        // Remove /. suffix(es)
+        while (normalizedPath.EndsWith("/."))
+            normalizedPath = normalizedPath.Substring(0, normalizedPath.Length - 2);
+        if (normalizedPath == string.Empty)
+            return "/";
+
+        // Remove unnecessary path components
+        normalizedPath = normalizedPath.Replace("/./", "/");
+
+        // Remove trailing separator
+        if (normalizedPath.EndsWith("/") && normalizedPath != "/")
+            normalizedPath = normalizedPath.Substring(0, normalizedPath.Length - 1);
 
         return normalizedPath;
     }
