1f8f7619477a4575c890c16c4d0c4dc93cc92e4d
Christoph Hausner <christoph_hausner@hotmail.de>
2025-05-23 20:34:10 +0200

    Refactor GetConanPackageInfo.ps1

--- GetConanPackageInfo.ps1
+++ GetConanPackageInfo.ps1
@@ -1,1 +1,23 @@
-﻿gci -rec conanfile.py | % { $cmakeTarget = $null; $cmakeFile = $null;  $c = get-content $_ -raw; if ($c -match "self\.cpp_info\.set_property\(`"cmake_target_name`", `"([^`"]*)`"\)") { $cmakeTarget = $Matches[1] } if ($c -match "self\.cpp_info\.set_property\(`"cmake_file_name`", `"([^`"]*)`"\)") { $cmakeFile = $Matches[1] } if ($cmakeTarget -or $cmakeFile) { $package = $_.Directory.Parent.Name ; "$package,$cmakeFile,$cmakeTarget" } }
+﻿git clone https://github.com/conan-io/conan-center-index.git
+Set-Location conan-center-index
+
+Get-ChildItem -Recurse conanfile.py | foreach { 
+    $packageName = $_.Directory.Parent.Name
+    $content = Get-Content $_ -Raw
+    
+    if ($content -match "self\.cpp_info\.set_property\(`"cmake_target_name`", `"([^`"]*)`"\)") { 
+        $cmakeTargetName = $Matches[1]
+    } else {
+        $cmakeTargetName = $null
+    }
+
+    if ($content -match "self\.cpp_info\.set_property\(`"cmake_file_name`", `"([^`"]*)`"\)") {
+        $cmakeFileName = $Matches[1] 
+    } else {
+        $cmakeFileName = $null
+    }
+
+    if ($cmakeTargetName -or $cmakeFileName) { 
+        "$packageName,$cmakeFileName,$cmakeTargetName"
+    } 
+}
