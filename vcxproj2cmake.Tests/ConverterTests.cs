703895c278cc959bfd19ef08c8db90642b6ce4e6
Christoph Hausner <christoph_hausner@hotmail.de>
2025-06-08 23:40:40 +0200

    Fix tests on Linux

--- vcxproj2cmake.Tests/ConverterTests.cs
+++ vcxproj2cmake.Tests/ConverterTests.cs
@@ -90,16 +90,16 @@ public class ConverterTests
             var fileSystem = new MockFileSystem();
             fileSystem.Directory.SetCurrentDirectory(Environment.CurrentDirectory);
 
-            fileSystem.AddFile(@"EmptyProject1/EmptyProject1.vcxproj", new(TestData.EmptyProject));
-            fileSystem.AddFile(@"EmptyProject2/EmptyProject2.vcxproj", new(TestData.EmptyProject));
+            fileSystem.AddFile(@"EmptyProject1\EmptyProject1.vcxproj", new(TestData.EmptyProject));
+            fileSystem.AddFile(@"EmptyProject2\EmptyProject2.vcxproj", new(TestData.EmptyProject));
 
             fileSystem.AddFile(@"TwoEmptyProjects.sln", new("""                
                 Microsoft Visual Studio Solution File, Format Version 12.00
                 # Visual Studio Version 17
                 # MinimumVisualStudioVersion = 10.0.40219.1
-                Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmptyProject1", "EmptyProject1/EmptyProject1.vcxproj", "{11111111-1111-1111-1111-111111111111}"
+                Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmptyProject1", "EmptyProject1\EmptyProject1.vcxproj", "{11111111-1111-1111-1111-111111111111}"
                 EndProject
-                Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmptyProject2", "EmptyProject2/EmptyProject2.vcxproj", "{22222222-2222-2222-2222-222222222222}"
+                Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmptyProject2", "EmptyProject2\EmptyProject2.vcxproj", "{22222222-2222-2222-2222-222222222222}"
                 EndProject
                 """));
 
@@ -124,7 +124,7 @@ public class ConverterTests
                 add_subdirectory(EmptyProject2)
                 """);
 
-            AssertEx.FileHasContent(@"EmptyProject1/CMakeLists.txt", fileSystem, """            
+            AssertEx.FileHasContent(@"EmptyProject1\CMakeLists.txt", fileSystem, """            
                 cmake_minimum_required(VERSION 3.13)
                 project(EmptyProject1)
 
@@ -148,7 +148,7 @@ public class ConverterTests
                 )
                 """);
 
-            AssertEx.FileHasContent(@"EmptyProject2/CMakeLists.txt", fileSystem, """            
+            AssertEx.FileHasContent(@"EmptyProject2\CMakeLists.txt", fileSystem, """            
                 cmake_minimum_required(VERSION 3.13)
                 project(EmptyProject2)
 
@@ -281,16 +281,16 @@ public class ConverterTests
             var fileSystem = new MockFileSystem();
             fileSystem.Directory.SetCurrentDirectory(Environment.CurrentDirectory);
 
-            fileSystem.AddFile(@"EmptyProject1/EmptyProject1.vcxproj", new(TestData.EmptyProject));
-            fileSystem.AddFile(@"EmptyProject2/EmptyProject2.vcxproj", new(TestData.EmptyProject));
+            fileSystem.AddFile(@"EmptyProject1\EmptyProject1.vcxproj", new(TestData.EmptyProject));
+            fileSystem.AddFile(@"EmptyProject2\EmptyProject2.vcxproj", new(TestData.EmptyProject));
 
             fileSystem.AddFile(@"TwoEmptyProjects.sln", new("""                
                 Microsoft Visual Studio Solution File, Format Version 12.00
                 # Visual Studio Version 17
                 # MinimumVisualStudioVersion = 10.0.40219.1
-                Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmptyProject1", "EmptyProject1/EmptyProject1.vcxproj", "{11111111-1111-1111-1111-111111111111}"
+                Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmptyProject1", "EmptyProject1\EmptyProject1.vcxproj", "{11111111-1111-1111-1111-111111111111}"
                 EndProject
-                Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmptyProject2", "EmptyProject2/EmptyProject2.vcxproj", "{22222222-2222-2222-2222-222222222222}"
+                Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmptyProject2", "EmptyProject2\EmptyProject2.vcxproj", "{22222222-2222-2222-2222-222222222222}"
                 EndProject
                 """));
 
@@ -309,11 +309,11 @@ public class ConverterTests
 
             // Assert
             var logs = string.Join(Environment.NewLine, logger.Messages);
-            Assert.Contains($"Generated output for {Path.Combine(Environment.CurrentDirectory, @"EmptyProject1\CMakeLists.txt")}", logs);
+            Assert.Contains($"Generated output for {Path.GetFullPath(@"EmptyProject1\CMakeLists.txt")}", logs);
             Assert.Contains("    add_executable(EmptyProject1", logs);
-            Assert.Contains($"Generated output for {Path.Combine(Environment.CurrentDirectory, @"EmptyProject2\CMakeLists.txt")}", logs);
+            Assert.Contains($"Generated output for {Path.GetFullPath(@"EmptyProject2\CMakeLists.txt")}", logs);
             Assert.Contains("    add_executable(EmptyProject2", logs);
-            Assert.Contains($"Generated output for {Path.Combine(Environment.CurrentDirectory, "CMakeLists.txt")}", logs);
+            Assert.Contains($"Generated output for {Path.GetFullPath("CMakeLists.txt")}", logs);
             Assert.Contains("    add_subdirectory(EmptyProject1)", logs);
             Assert.Contains("    add_subdirectory(EmptyProject2)", logs);
 
