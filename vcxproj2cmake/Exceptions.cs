d9fa3d85f2ee9292ac832f59bf13a1ab42ac7ae5
Christoph Hausner <christoph_hausner@hotmail.de>
2025-06-08 16:56:52 +0200

    Move CatastrophicFailureException into its own file

---
+++ vcxproj2cmake/Exceptions.cs
@@ -0,0 +1,8 @@
+﻿namespace vcxproj2cmake;
+
+public class CatastrophicFailureException : Exception
+{
+    public CatastrophicFailureException() { }
+    public CatastrophicFailureException(string message) : base(message) { }
+    public CatastrophicFailureException(string message, Exception inner) : base(message, inner) { }
+}
--- vcxproj2cmake/Program.cs
+++ vcxproj2cmake/Program.cs
@@ -154,10 +154,3 @@ static class Program
         }
     }
 }
-
-public class CatastrophicFailureException : Exception
-{
-    public CatastrophicFailureException() { }
-    public CatastrophicFailureException(string message) : base(message) { }
-    public CatastrophicFailureException(string message, Exception inner) : base(message, inner) { }
-}
