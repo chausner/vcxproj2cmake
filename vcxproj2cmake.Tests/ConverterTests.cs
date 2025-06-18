72b5ad0b87db9c80a4e28c0d1dd23b90915f3259
chausner <15180557+chausner@users.noreply.github.com>
2025-06-18 22:59:15 +0200

    Add tests for target_link_libraries output (#26)

--- vcxproj2cmake.Tests/ConverterTests.cs
+++ vcxproj2cmake.Tests/ConverterTests.cs
@@ -1148,6 +1148,120 @@ public class ConverterTests
         }
     }
 
+    public class AdditionalDependenciesTests
+    {
+        [Fact]
+        public void Given_ProjectWithAdditionalDependencies_When_Converted_Then_LibrariesAreLinked()
+        {
+            var fileSystem = new MockFileSystem();
+            fileSystem.Directory.SetCurrentDirectory(Environment.CurrentDirectory);
+
+            fileSystem.AddFile(@"Project.vcxproj", new("""
+                <?xml version="1.0" encoding="utf-8"?>
+                <Project DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+                    <ItemGroup Label="ProjectConfigurations">
+                        <ProjectConfiguration Include="Debug|Win32">
+                            <Configuration>Debug</Configuration>
+                            <Platform>Win32</Platform>
+                        </ProjectConfiguration>
+                        <ProjectConfiguration Include="Release|Win32">
+                            <Configuration>Release</Configuration>
+                            <Platform>Win32</Platform>
+                        </ProjectConfiguration>
+                    </ItemGroup>
+                    <PropertyGroup>
+                        <ConfigurationType>Application</ConfigurationType>
+                    </PropertyGroup>
+                    <ItemDefinitionGroup>
+                        <Link>
+                            <AdditionalDependencies>Foo.lib;Bar.lib;%(AdditionalDependencies)</AdditionalDependencies>
+                        </Link>
+                    </ItemDefinitionGroup>
+                </Project>
+                """));
+
+            var converter = new Converter(fileSystem, NullLogger.Instance);
+
+            converter.Convert(
+                projectFiles: [new(@"Project.vcxproj")],
+                solutionFile: null,
+                qtVersion: null,
+                enableStandaloneProjectBuilds: false,
+                indentStyle: "spaces",
+                indentSize: 4,
+                dryRun: false);
+
+            var cmake = fileSystem.GetFile(@"CMakeLists.txt").TextContents;
+            Assert.Contains(
+                """
+                target_link_libraries(Project
+                    PUBLIC
+                        Foo.lib
+                        Bar.lib
+                )
+                """.Trim(),
+                cmake);
+        }
+
+        [Fact]
+        public void Given_ProjectWithConfigSpecificAdditionalDependencies_When_Converted_Then_GeneratorExpressionsUsed()
+        {
+            var fileSystem = new MockFileSystem();
+            fileSystem.Directory.SetCurrentDirectory(Environment.CurrentDirectory);
+
+            fileSystem.AddFile(@"Project.vcxproj", new("""
+                <?xml version="1.0" encoding="utf-8"?>
+                <Project DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+                    <ItemGroup Label="ProjectConfigurations">
+                        <ProjectConfiguration Include="Debug|Win32">
+                            <Configuration>Debug</Configuration>
+                            <Platform>Win32</Platform>
+                        </ProjectConfiguration>
+                        <ProjectConfiguration Include="Release|Win32">
+                            <Configuration>Release</Configuration>
+                            <Platform>Win32</Platform>
+                        </ProjectConfiguration>
+                    </ItemGroup>
+                    <PropertyGroup>
+                        <ConfigurationType>Application</ConfigurationType>
+                    </PropertyGroup>
+                    <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+                        <Link>
+                            <AdditionalDependencies>Foo_d.lib;%(AdditionalDependencies)</AdditionalDependencies>
+                        </Link>
+                    </ItemDefinitionGroup>
+                    <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+                        <Link>
+                            <AdditionalDependencies>Foo.lib;%(AdditionalDependencies)</AdditionalDependencies>
+                        </Link>
+                    </ItemDefinitionGroup>
+                </Project>
+                """));
+
+            var converter = new Converter(fileSystem, NullLogger.Instance);
+
+            converter.Convert(
+                projectFiles: [new(@"Project.vcxproj")],
+                solutionFile: null,
+                qtVersion: null,
+                enableStandaloneProjectBuilds: false,
+                indentStyle: "spaces",
+                indentSize: 4,
+                dryRun: false);
+
+            var cmake = fileSystem.GetFile(@"CMakeLists.txt").TextContents;
+            Assert.Contains(
+                """
+                target_link_libraries(Project
+                    PUBLIC
+                        $<$<CONFIG:Debug>:Foo_d.lib>
+                        $<$<CONFIG:Release>:Foo.lib>
+                )
+                """.Trim(),
+                cmake);
+        }
+    }
+
     public class OpenMPSupportTests
     {
         static string CreateProject(string debugValue, string releaseValue) => $"""
