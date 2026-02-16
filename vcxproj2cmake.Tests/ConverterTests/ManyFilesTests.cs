0428bc4498b217eade9e80962ca93d45b4328219
Christoph Hausner <christoph_hausner@hotmail.de>
2026-02-16 18:56:35 +0100

    Add ManyFilesTest

---
+++ vcxproj2cmake.Tests/ConverterTests/ManyFilesTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace vcxproj2cmake.Tests;
+
+public partial class ConverterTests
+{
+    public class ManyFilesTests
+    {
+        static string CreateProject(params string[] sources)
+            => $"""
+        <?xml version="1.0" encoding="utf-8"?>
+        <Project DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+            <ItemGroup Label="ProjectConfigurations">
+                <ProjectConfiguration Include="Debug|Win32">
+                    <Configuration>Debug</Configuration>
+                    <Platform>Win32</Platform>
+                </ProjectConfiguration>
+                <ProjectConfiguration Include="Release|Win32">
+                    <Configuration>Release</Configuration>
+                    <Platform>Win32</Platform>
+                </ProjectConfiguration>
+            </ItemGroup>
+            <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'" Label="Configuration">
+                <UseDebugLibraries>true</UseDebugLibraries>
+            </PropertyGroup>
+            <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'" Label="Configuration">
+                <UseDebugLibraries>false</UseDebugLibraries>
+            </PropertyGroup>
+            {(sources.Length > 0 ? $"""
+            <ItemGroup>
+                {string.Join(Environment.NewLine, sources.Select(s => $"                <ClCompile Include=\"{s}\" />"))}
+            </ItemGroup>
+            """ : string.Empty)}
+        </Project>
+        """;
+
+        [Fact]
+        public void Given_ProjectWithManySources_When_Converted_Then_NoErrorOccurs()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem();
+            fileSystem.Directory.SetCurrentDirectory(Environment.CurrentDirectory);
+
+            var sources = Enumerable.Range(1, 1500).Select(i => $"Source{i}.cpp").ToArray();
+            fileSystem.AddFile(@"Project.vcxproj", new(CreateProject(sources)));
+
+            var converter = new Converter(fileSystem, NullLogger.Instance);
+
+            // Act
+            converter.Convert(
+                projectFiles: [new(@"Project.vcxproj")]);
+
+            // Assert
+            var cmake = fileSystem.GetFile(@"CMakeLists.txt").TextContents;
+            foreach (var source in sources)            
+                Assert.Contains(source, cmake);            
+        }
+    }
+}
