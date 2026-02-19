7f54f92fbc15a623b031f20509f90f3c6c1f51f6
Christoph Hausner <christoph_hausner@hotmail.de>
2026-02-19 18:30:12 +0100

    Tweak order of statements in GetConanPackageInfo.ps1

--- Scripts/GetConanPackageInfo.ps1
+++ Scripts/GetConanPackageInfo.ps1
@@ -6,16 +6,16 @@ Get-ChildItem conan-center-index -Filter conanfile.py -Recurse | foreach {
     $packageName = $_.Directory.Parent.Name
     $content = Get-Content $_ -Raw
     
-    if ($content -match "self\.cpp_info\.set_property\(`"cmake_target_name`", `"([^`"]*)`"\)") { 
-        $cmakeTargetName = $Matches[1]
-    } else {
-        $cmakeTargetName = $null
-    }
-
     if ($content -match "self\.cpp_info\.set_property\(`"cmake_file_name`", `"([^`"]*)`"\)") {
         $cmakeFileName = $Matches[1] 
     } else {
         $cmakeFileName = $null
+    }
+    
+    if ($content -match "self\.cpp_info\.set_property\(`"cmake_target_name`", `"([^`"]*)`"\)") { 
+        $cmakeTargetName = $Matches[1]
+    } else {
+        $cmakeTargetName = $null
     }
 
     if ($packageName -in "cmake_package","header_only") {
